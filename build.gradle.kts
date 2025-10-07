0f8bd108814f2f2b1555c40c43c7f0f14e51e24b
Artem Shykhov <shykhovartem@gmail.com>
2025-10-07 23:34:09 +0300

    chore: restructure of dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,39 +25,52 @@ repositories {
 }
 
 dependencies {
+    // Spring Boot
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.11")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // API Documentation
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.11")
+
+    // Database
     runtimeOnly("org.postgresql:postgresql")
 
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
+    // Testing - Core
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(module = "mockito-core")
     }
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:2.0.21")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Testing - Kotest
     testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     testImplementation("io.kotest:kotest-property:5.9.1")
     testImplementation("io.kotest:kotest-framework-datatest:5.9.1")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
+
+    // Testing - Mocking
     testImplementation("io.mockk:mockk:1.13.13")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+
+    // Testing - Testcontainers
     testImplementation("org.testcontainers:testcontainers:1.20.3")
     testImplementation("org.testcontainers:junit-jupiter:1.20.3")
     testImplementation("org.testcontainers:postgresql:1.19.6")
-    testImplementation("com.tngtech.archunit:archunit:1.2.1")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.reflections:reflections:0.10.2")
     testImplementation("org.testcontainers:kafka:1.21.1")
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
-    // Architectural testing
+
+    // Testing - Architecture
+    testImplementation("com.tngtech.archunit:archunit:1.2.1")
     testImplementation("com.lemonappdev:konsist:0.17.3")
+
+    // Testing - Utilities
+    testImplementation("org.reflections:reflections:0.10.2")
 }
 
 kotlin {
